6c4458a9fda4b57b092f0b0d31ea288cf44c2735
Francisco Jacinto <l17120242@morelia.tecnm.mx>
2022-05-22 16:42:23 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/itm/link/basicfirebaseapp/Card.kt
@@ -0,0 +1,7 @@
+package mx.edu.itm.link.basicfirebaseapp
+
+class Card(valor: String, color: String, palo: String) {
+    val valor = valor
+    val color = color
+    val palo = palo
+}
---
+++ app/src/main/java/mx/edu/itm/link/basicfirebaseapp/User.kt
@@ -0,0 +1,8 @@
+package mx.edu.itm.link.basicfirebaseapp
+
+class User(nombre: String, apellido: String, edad: Int, email: String) {
+    val nombre = nombre
+    val apellido = apellido
+    val edad = edad
+    val email = email
+}
